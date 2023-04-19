dfb2ed20c14a9f651e68d86e76a4dedd530bd497
Daria Utkina <daria_utkina_ej@gmail.com>
2023-04-19 06:06:55 +0300

    add post method

--- app.js
+++ app.js
@@ -4,9 +4,33 @@
 const express = require("express");
 const app = express();
 
+/** Подключаем Handlebar */
 const hbs = require("hbs");
 
+/** Подключаем Multer */
+const multer = require("multer");
+
+/** Сохраняем в файл Files */
+const upload = multer({dest: "files"});
+
+/** Зададим конфигурацию для мультера */
+const StorageConfig = multer.diskStorage(
+    {
+        destination: (req, file, cb) =>{
+            cb(null, "files")
+        },
+        filename: (req, file, cb) =>{
+            cb(null, Date.now() + "-" + file.originalname)
+        }
+    }
+);
+
 app.set("view engine", "hbs");
+
+app.use(express.static(__dirname));
+
+/** подключаем конфигурацию для мультера */
+app.use(multer({storage:StorageConfig}).single("filedata"));
 
 /**
  * Подключаем виджет меню и все детали, которые будут в partial
@@ -37,6 +61,22 @@ app.get("/upload", function(request, response){
 
     });
 })
+
+/**
+ * Метод загрузки файлов Post'ом.
+ */
+app.post("/upload", upload.single("filedata") , function(request, response, next){
+   let filedata = request.file;
+   if (!filedata) response.send("Ошибка при загрузке файла, нет файла")
+    else
+       response.render("upload", {
+           title: "File upload",
+           buttonName: "Загрузить файл",
+
+       });
+});
+
+/** multer */
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
